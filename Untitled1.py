cacb6d20bb1b0c8cdb015ef6c8cf9c0664802659
me1234-5me <75872882+me1234-5me@users.noreply.github.com>
2022-04-01 12:22:35 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+student=pd.read_csv('student_scores.csv')
+
+
+# In[2]:
+
+
+student.head()
+
+
+# In[3]:
+
+
+student.info()
+
+
+# In[4]:
+
+
+
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=Train_test_split(x,y,relation_state=0)
+
+
+# In[5]:
+
+
+x=point[:,:1]
+y=point[:,:4]
+
+
+# In[6]:
+
+
+student.describe()
+
+
+# In[7]:
+
+
+x=point.iloc[:,0]
+y=point.iloc[:,4]
+
+
+# In[10]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+student=pd.read_csv('student_score.csv')
+from sklearn.model_selection import train_test_split
+
+
+# In[11]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd 
+student=pd.read_csv('student_score.csv')
+
+
+# In[12]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+student=pd.read_csv('student_scores.csv')
+
+
+# In[13]:
+
+
+student.head()
+
+
+# In[14]:
+
+
+company=pd.read_csv('customer_data.csv')
+
+
+# In[16]:
+
+
+company.describe()
+
+
+# In[17]:
+
+
+company.head()
+
+
+# In[20]:
+
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+student=pd.read_csv('student_scores.csv')
+
+
+# In[21]:
+
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+student=pd.read_csv('student_scores.csv')
+company=pd.read_csv('custmer_data.csv')
+
+
+# In[22]:
+
+
+student=pd.read_csv('student_scores.csv')
+
+
+# In[23]:
+
+
+student.head()
+
+
+# In[26]:
+
+
+y_pred=student.predict(x_test)
+
+
+# In[25]:
+
+
+company.head()
+
+
+# In[27]:
+
+
+from sklearn.linear_model import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,classification_state=0)
+
+
+# In[ ]:
+
+
+from 
+
